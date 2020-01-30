fefb108e484149e85e881f77dc15f8eb6b3fa8c0
Levon Rose <553185@ACDM.DS.SAIT.CA>
2020-01-30 16:46:25 -0700

    Create RegisterAccount.java

---
+++ src/java/rest/RegisterAccount.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rest;
+
+import java.io.StringReader;
+import javax.json.Json;
+import javax.json.stream.JsonParser;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+import javax.ws.rs.core.MediaType;
+import services.AccountService;
+
+/**
+ * REST Web Service
+ *
+ * @author 553185
+ */
+@Path("register")
+public class RegisterAccount {
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of RegisterAccount
+     */
+    public RegisterAccount() {
+    }
+
+    /**
+     * Retrieves representation of an instance of rest.RegisterAccount
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getJson() {
+        //TODO return proper representation object
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * PUT method for updating or creating an instance of RegisterAccount
+     * @param content representation for the resource
+     */
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    public String putJson(String content) {
+        System.out.println(content);
+        
+        JsonParser parser = Json.createParser(new StringReader(content));
+        
+        JsonParser.Event event = parser.next(); // START_OBJECT
+        event = parser.next();       // KEY_NAME
+        event = parser.next();       // VALUE_STRING
+        String parsedName = parser.getString();
+        
+        event = parser.next();       // KEY_NAME
+        event = parser.next();       // VALUE_STRING
+        String parsedPassword = parser.getString();
+        
+        event = parser.next();       // KEY_NAME
+        event = parser.next();       // VALUE_STRING
+        String parsedFirstName = parser.getString();
+        
+        event = parser.next();       // KEY_NAME
+        event = parser.next();       // VALUE_STRING
+        String parsedLastName = parser.getString();
+        
+        event = parser.next();       // KEY_NAME
+        event = parser.next();       // VALUE_STRING
+        String parsedEmail = parser.getString();
+        
+        System.out.println(parsedName);
+        System.out.println(parsedPassword);
+        System.out.println(parsedFirstName);
+        System.out.println(parsedLastName);
+        System.out.println(parsedEmail);
+
+        AccountService as = new AccountService();
+        boolean registered = false;
+        
+        registered = as.register(parsedName, parsedPassword, parsedFirstName, parsedLastName, parsedEmail);
+        
+        if(registered){
+            return "account registered";
+        }
+        return "Username already taken.";
+    }
+}
