080d2f4198f20a30e98238be41b0d6f4fe1479af
Hans Cabrera <44388744+Hawns4@users.noreply.github.com>
2020-02-06 09:39:52 -0700

    removed unused package imports

--- src/java/models/User.java
+++ src/java/models/User.java
@@ -1,14 +1,5 @@
 package models;
 
-import database.ConnectionPool;
-import database.NotesDBException;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 public class User {
     
     private String username;
