f5fc41d97ed3db6171ab24f49fbbbdbcd6b3c29a
g-priime <57049481+g-priime@users.noreply.github.com>
2020-02-29 15:15:40 -0700

    Update Login.java

--- src/java/rest/Login.java
+++ src/java/rest/Login.java
@@ -59,7 +59,7 @@ public class Login {
         AccountService as = new AccountService();
         
         if (as.login(username,password)) {
-          return JWT.createJWT("testID", "testIssuer", username, 86400000);
+          return JWT.createJWT(username, 86400000);
         } else {
             return null;
         }
