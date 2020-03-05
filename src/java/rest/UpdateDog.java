5ee02f7b7aea8a116145327c18bdaea648ae1387
Carsen Johns <640195@ACDM.DS.SAIT.CA>
2020-03-05 10:11:08 -0700

    updatedog

--- src/java/rest/UpdateDog.java
+++ src/java/rest/UpdateDog.java
@@ -5,7 +5,18 @@
  */
 package rest;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import io.jsonwebtoken.Claims;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import models.Dog;
+import models.User;
+import services.AccountService;
+import services.DogService;
+import services.JWT;
 
 /**
  *
@@ -13,5 +24,24 @@ import javax.ws.rs.Path;
  */
 @Path("updateDog")
 public class UpdateDog {
-    
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    public String putJson(String content) {
+        String token = JWT.getToken(content);
+        //return the token decoded
+         Claims claims;
+        try{
+        claims = JWT.decodeJWT(token);
+        } catch(Exception e){
+            return "Authentication error, bad token";
+        } 
+        Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd").create();
+        Dog dog = gson.fromJson(content, Dog.class);
+        DogService ds = new DogService();
+        if (ds.updateDog(dog)) {
+            return "Updated";
+        }
+        
+        return "Not Updated";
+    }
 }
