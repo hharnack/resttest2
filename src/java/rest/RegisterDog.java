702202e282774348afeab4cccfbd591447aa511e
Viktoriia Davydova <769217@ACDM.DS.SAIT.CA>
2020-03-05 17:29:22 -0700

    Update RegisterDog.java

--- src/java/rest/RegisterDog.java
+++ src/java/rest/RegisterDog.java
@@ -9,8 +9,10 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import io.jsonwebtoken.Claims;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.UriInfo;
@@ -35,6 +37,19 @@ public class RegisterDog {
     private UriInfo context;
 
     /**
+     * Retrieves representation of an instance of rest.CheckAccount
+     *
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getJson() {
+        //TODO return proper representation object
+        //throw new UnsupportedOperationException();
+        return "Valid";
+    }
+    
+    /**
      * PUT method for updating or creating an instance of
      *
      * @param token
@@ -44,6 +59,7 @@ public class RegisterDog {
     @PUT
     @Consumes(MediaType.APPLICATION_JSON)
     public String putJson(String content) {
+        System.out.println(content);
         //gets the token from the json
         String token = JWT.getToken(content);
         //return the token decoded
