21cc02a1a1ae044cae3517d0854862a164859704
Hawns4 <44388744+Hawns4@users.noreply.github.com>
2020-03-07 14:06:44 -0700

    fixed

--- src/java/rest/Base.java
+++ src/java/rest/Base.java
@@ -5,7 +5,6 @@
  */
 package rest;
 
-import io.jsonwebtoken.Claims;
 import java.io.StringReader;
 import javax.json.Json;
 import javax.json.stream.JsonParser;
@@ -13,12 +12,10 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.UriInfo;
-import services.JWT;
 
 /**
  * Just so I (Hans) can copy and paste stuff
@@ -55,15 +52,9 @@ public class Base {
      */
     @PUT
     @Consumes(MediaType.APPLICATION_JSON)
-    public String putJson(@PathParam("token") String token) {
-        String token = JWT.getToken(token);
-        Claims claims;
-        try {
-            claims = JWT.decodeJWT(token);
-        } catch (Exception e) {
-            return null;
-        }
-        String username = claims.get("username", String.class);
+    public String putJson(String content) {
+        JsonParser parser = Json.createParser(new StringReader(content));
+        JsonParser.Event event = parser.next(); // START_OBJECT
         
         return null;
     }
--- src/java/rest/GetDog.java
+++ src/java/rest/GetDog.java
@@ -13,6 +13,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
@@ -42,8 +43,7 @@ public class GetDog {
      */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Dog getJson(String content) {
-        String token = JWT.getToken(content);
+    public Dog getJson(@PathParam("token") String token) {
         Claims claims;
         try {
             claims = JWT.decodeJWT(token);
