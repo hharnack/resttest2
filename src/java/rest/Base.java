ae8740418159871fba1742fd7773d6de61597fb0
Hawns4 <44388744+Hawns4@users.noreply.github.com>
2020-03-07 14:05:32 -0700

    Update Base.java

--- src/java/rest/Base.java
+++ src/java/rest/Base.java
@@ -13,6 +13,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
@@ -54,8 +55,8 @@ public class Base {
      */
     @PUT
     @Consumes(MediaType.APPLICATION_JSON)
-    public String putJson(String content) {
-        String token = JWT.getToken(content);
+    public String putJson(@PathParam("token") String token) {
+        String token = JWT.getToken(token);
         Claims claims;
         try {
             claims = JWT.decodeJWT(token);
