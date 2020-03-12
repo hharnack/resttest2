2f876d0e47f6c890e592c69612fe77b04f4b2c3c
Hawns4 <44388744+Hawns4@users.noreply.github.com>
2020-03-12 10:08:59 -0600

    delete

--- src/java/rest/DeleteDog.java
+++ src/java/rest/DeleteDog.java
@@ -6,8 +6,12 @@
 package rest;
 
 import io.jsonwebtoken.Claims;
+import java.io.StringReader;
+import javax.json.Json;
+import javax.json.stream.JsonParser;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 import services.DogService;
 import services.JWT;
@@ -16,12 +20,22 @@ import services.JWT;
  *
  * @author 703174
  */
+@Path("deleteDog")
 public class DeleteDog {
     
     @PUT
     @Consumes(MediaType.APPLICATION_JSON)
     public String putJson(String content) {
-        String token = JWT.getToken(content);
+        JsonParser parser = Json.createParser(new StringReader(content));
+
+        parser.next(); // START_OBJECT
+
+        parser.next(); // KEY_NAME
+        parser.next(); // VALUE_STRING
+        String token = parser.getString();
+        parser.next(); // KEY_NAME
+        parser.next(); // VALUE_STRING
+        int petID = parser.getInt();
         //return the token decoded
         try{
         Claims claims = JWT.decodeJWT(token);
@@ -29,10 +43,10 @@ public class DeleteDog {
             return "Authentication error, bad token";
         }
         
-        if (new DogService().delete(0)) {
-            // chef boyardee
+        if (new DogService().delete(petID)) {
+            return "yes";
         }
        
-        return "a";
+        return "no";
     }
 }
